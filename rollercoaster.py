b480ef0f538643c70289689bfcfbfcc190fcdf4b
gurreric@gmail.com <gurreric@gmail.com>
2024-01-02 10:01:03 -0500

    added if loo

--- rollercoaster.py
+++ rollercoaster.py
@@ -1,2 +1,9 @@
 # Roller Coaster
 print("Welcome to the roller coaster app!")
+#declare variables
+height = int(input("What is you heigh in cm?"))
+
+if height > 100:
+    print("you're tall enough")
+else:
+    print("sorry, moose out front should have told ya.")
