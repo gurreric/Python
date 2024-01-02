70401d92afff59c8b9237ced97c2939cfbe9442c
gurreric@gmail.com <gurreric@gmail.com>
2024-01-02 10:02:43 -0500

    Added coment

--- rollercoaster.py
+++ rollercoaster.py
@@ -3,6 +3,8 @@ print("Welcome to the roller coaster app!")
 #declare variables
 height = int(input("What is you heigh in cm?"))
 
+# Check for height:
+
 if height > 100:
     print("you're tall enough")
 else:
