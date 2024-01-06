4e3fc4981a3d205d10ae1daf5946662d71637e9b
gurreric@gmail.com <gurreric@gmail.com>
2024-01-06 09:44:50 -0500

    final treasure

--- treasure.py
+++ treasure.py
@@ -1,13 +1,13 @@
 print("Welcome to Treasure Isle.")
-firststep =input("Do you wish to go left or right?").lower()
-if firststep =="left":
-    print("You're on the right track!")
-    swim =input("Do you want to swim or wait?").lower()
-    if swim =="swim":
-        print("Welp, you dead.")
-    else:
-        print("Good idea. Boat is here.")
-else:
-    print("You fell into a hold.") 
+# firststep =input("Do you wish to go left or right?").lower()
+# if firststep =="left":
+#     print("You're on the right track!")
+#     swim =input("Do you want to swim or wait?").lower()
+#     if swim =="swim":
+#         print("Welp, you dead.")
+#     else:
+#         print("Good idea. Boat is here.")
+# else:
+#     print("You fell into a hold.") 
 
 
