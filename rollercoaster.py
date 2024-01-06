54090d9c80c17c6c75de9f9c71c3428f212a169a
gurreric@gmail.com <gurreric@gmail.com>
2024-01-06 09:36:39 -0500

    treasure first

--- rollercoaster.py
+++ rollercoaster.py
@@ -2,10 +2,20 @@
 print("Welcome to the roller coaster app!")
 #declare variables
 height = int(input("What is you heigh in cm?"))
-
+age = int(input("What is your age?"))
+price =float()
 # Check for height:
-
-if height > 100:
+# Build nested if statement
+if height >= 120:
     print("you're tall enough")
+    if age <= 18:
+        print("Your ticket is $7")
+
+    else:
+        price = 12
+        print(f"Your ticket costs {price}")
+
+
+
 else:
     print("sorry, moose out front should have told ya.")
