1af1776316bc38fb2e0313418782f18b501d6c54
gurreric <gurreric@gmail.com>
2024-02-26 08:11:28 -0500

    Delete input.py

--- input.py
+++
@@ -1,9 +0,0 @@
-#This is input
-# print("Hello, "+ input("What is your name?"))
-# To add that hashtag for comments you can also use ctrl /
-
-# Now from the day 3 auditorium
-name1 =input("What is your name?")
-name2 = input("What is the second name?")
-print("The names are: " + name1 +' ' + name2)
-print(len(name1))
